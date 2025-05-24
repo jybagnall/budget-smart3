fde41c3cae58dffd41b6d085587ff2296a031d19
jybagnall <jybagnall@gmail.com>
2025-05-24 19:08:51 +0900

    Fix auth error2

--- app/_services/auth.js
+++ app/_services/auth.js
@@ -1,57 +1,45 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { createUser, getUser } from "./user.data-service";
-import {
-  auth as nextAuth,
-  signIn as nextSignIn,
-  signOut as nextSignOut,
-} from "next-auth";
-
-const { AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET, NEXTAUTH_SECRET, NEXTAUTH_URL } =
-  process.env;
-
-if (!AUTH_GOOGLE_ID || !AUTH_GOOGLE_SECRET || !NEXTAUTH_SECRET) {
-  throw new Error("Missing required env vars for NextAuth.");
-}
 
 const authConfig = {
   providers: [
-    GoogleProvider({
-      clientId: AUTH_GOOGLE_ID,
-      clientSecret: AUTH_GOOGLE_SECRET,
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
-  secret: NEXTAUTH_SECRET,
   callbacks: {
+    authorized({ auth }) {
+      return !!auth?.user;
+    },
+
     async signIn({ user }) {
       try {
         const existingUser = await getUser(user.email);
-        if (!existingUser) {
+
+        if (!existingUser)
           await createUser({
             email: user.email,
             name: user.name,
             google_id: user.id,
           });
-        }
+
         return true;
-      } catch (err) {
-        console.error("signIn callback error:", err);
+      } catch {
         return false;
       }
     },
+
     async session({ session }) {
-      try {
-        const loggedInUser = await getUser(session.user.email);
-        if (!loggedInUser) return null;
-        session.user.user_id = loggedInUser.google_id;
-        return session;
-      } catch (err) {
-        console.error("session callback error:", err);
+      const loggedIn_user = await getUser(session.user.email);
+
+      if (!loggedIn_user) {
         return null;
       }
-    },
-    authorized({ auth }) {
-      return !!auth?.user;
+
+      session.user.user_id = loggedIn_user.google_id;
+      return session;
     },
   },
   pages: {
@@ -59,9 +47,9 @@ const authConfig = {
   },
 };
 
-const handler = NextAuth(authConfig);
-
-export const GET = handler;
-export const POST = handler;
-
-export { nextAuth as auth, nextSignIn as signIn, nextSignOut as signOut };
+export const {
+  auth,
+  signIn,
+  signOut,
+  handlers: { GET, POST },
+} = NextAuth(authConfig);
