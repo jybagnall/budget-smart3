4ba2ef5fbd57339c931af0a9a4d201ea1b1d35ad
jybagnall <jybagnall@gmail.com>
2025-05-24 18:33:13 +0900

    Fix NextAuth config

--- app/_services/auth.js
+++ app/_services/auth.js
@@ -56,60 +56,7 @@ const authConfig = {
 
 const handler = NextAuth(authConfig);
 
-export const auth = handler.auth;
-export const signIn = handler.signIn;
-export const signOut = handler.signOut;
 export const GET = handler;
 export const POST = handler;
 
-// const authConfig = {
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.AUTH_GOOGLE_ID,
-//       clientSecret: process.env.AUTH_GOOGLE_SECRET,
-//     }),
-//   ],
-//   callbacks: {
-//     authorized({ auth }) {
-//       return !!auth?.user;
-//     },
-
-//     async signIn({ user }) {
-//       try {
-//         const existingUser = await getUser(user.email);
-
-//         if (!existingUser)
-//           await createUser({
-//             email: user.email,
-//             name: user.name,
-//             google_id: user.id,
-//           });
-
-//         return true;
-//       } catch {
-//         return false;
-//       }
-//     },
-
-//     async session({ session }) {
-//       const loggedIn_user = await getUser(session.user.email);
-
-//       if (!loggedIn_user) {
-//         return null;
-//       }
-
-//       session.user.user_id = loggedIn_user.google_id;
-//       return session;
-//     },
-//   },
-//   pages: {
-//     signIn: "/login",
-//   },
-// };
-
-// export const {
-//   auth,
-//   signIn,
-//   signOut,
-//   handlers: { GET, POST },
-// } = NextAuth(authConfig);
+export { nextAuth as auth, nextSignIn as signIn, nextSignOut as signOut };
